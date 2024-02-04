ca986c65ae326ed46be71b25c369d9591b424aeb
olga-kri <krivenkoolga1987@gmail.com>
2024-02-04 20:10:51 +0500

    Initial commit

---
+++ src/main/java/pro/sky/CalculatorTest/CalculatorTestApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.CalculatorTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorTestApplication.class, args);
+	}
+
+}
